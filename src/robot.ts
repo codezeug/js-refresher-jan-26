0fbb0f2ed9b9fd0f8ac3454b400ca261f0cbafcc
Gabe Johnson <gabe.johnson@gmail.com>
2026-01-14 09:27:16 -0700

    Initial commit

---
+++ src/robot.ts
@@ -0,0 +1,6 @@
+// Blank on purpose!
+// First, run `npm i` to get the dependencies
+// Write something in here like:
+// console.log("Hey")
+// and then run it on the CLI with `npx ts-node src/robot.ts`.
+// It should print out Hey, like magic ✨🦄
